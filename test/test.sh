4cdb8220a0bfa7f4e533aa3042a12c420ff0b721
Shun Usami <usatie@gmail.com>
2025-01-17 02:34:52 +0900

    Modify the test script to display the result of each test and to display "OK" or "KO"

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,9 @@
-#!/bin/bash -x
-diff <(../ft_nm hello) <(nm hello)
-diff <(../ft_nm absolute_symbol.o) <(nm absolute_symbol.o)
-diff <(../ft_nm ../ft_nm) <(nm ../ft_nm)
-diff <(../ft_nm ../src/main.o) <(nm ../src/main.o)
+#!/bin/bash -e
+echo "./ft_nm hello"
+diff <(../ft_nm hello) <(nm hello) && echo "OK" || echo "KO"
+echo "./ft_nm absolute_symbol.o"
+diff <(../ft_nm absolute_symbol.o) <(nm absolute_symbol.o) && echo "OK" || echo "KO"
+echo "./ft_nm ft_nm"
+diff <(../ft_nm ../ft_nm) <(nm ../ft_nm) && echo "OK" || echo "KO"
+echo "./ft_nm src/main.o"
+diff <(../ft_nm ../src/main.o) <(nm ../src/main.o) && echo "OK" || echo "KO"
